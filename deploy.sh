e5530a9e81f3313df3433d65e73bfc9fb00f0087
Salim Badakhchani <sbadakhc@gmail.com>
2019-10-22 09:23:30 +0100

    fixed deployment sxript

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# This will delete then recompile the Go app, rebuild the container, then deploy to minikube.
-# Please verify your environment and dependencies are configured before running this script and deleting the app
-# in the process. 
-
-
-# remove old compiled go app
+# Remove the old binary
 rm csrgo
 
 
-# delete any previous csrgo pod
+# Delete any previous csrgo pods
 kubectl delete pod csrgo --grace-period=5  -n default
 
 
@@ -21,7 +16,6 @@ kubectl create clusterrolebinding default-view \
    --clusterrole=view \
    --serviceaccount=default:default
 
-
 kubectl apply -f fabric8-rbac.yaml
 
 kubectl run -i -t csrgo \
@@ -30,7 +24,6 @@ kubectl run -i -t csrgo \
    --image-pull-policy=Never \
    --restart=Never \
    --replicas=1
-
 
 # kubectl logs csrgo
 
